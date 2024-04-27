fd5b294e1879b0299a0de73f21674c6dfa2761fe
Barros313 <gbarros1337@gmail.com>
2024-04-26 22:51:37 -0300

    Configured search page

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,9 +1,9 @@
 const homeNavbar = document.getElementById('navbar-home');
 const publishNavbar = document.getElementById('navbar-publish');
-const chatNavbar = document.getElementById('navbar-chat');
 const profileNavbar = document.getElementById('navbar-profile');
 const configNavbar = document.getElementById('navbar-config');
 const searchHeader = document.getElementById('header-search-button');
+const searchInput = document.getElementById('search-input');
 
 homeNavbar.addEventListener("click", () => {
     window.location.href = "home.html";
@@ -21,6 +21,18 @@ configNavbar.addEventListener("click", () => {
     window.location.href = "config.html";
 });
 
-searchHeader.addEventListener("click", () => {
-    window.location.href = "search.html";  
-})
+const validSearch = () => {
+    if (searchInput.value === null) {
+        alert("Digite um valor para pesquisar.");
+    } else {
+        const searchValue = searchInput.value;
+
+        localStorage.setItem("search-value", JSON.stringify(searchValue));
+
+        searchValue.value = "";
+
+        window.location.href = "search.html";
+    }
+};
+
+searchHeader.addEventListener("click", validSearch);
--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,5 +1,11 @@
 const searchBackButton = document.getElementById('search-back-button');
+const text = document.getElementById('text');
+
+const searchValue = JSON.parse(localStorage.getItem("search-value"));
+
+text.innerHTML = "Resultados para a pesquisa " + searchValue;
 
 searchBackButton.addEventListener("click", () => {
     window.location.href = "home.html";
 });
+
