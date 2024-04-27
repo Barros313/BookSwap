fcd62191afb6428e8e96e54ba84bba20c41bbfac
Barros313 <gbarros1337@gmail.com>
2024-04-26 22:22:17 -0300

    Added Giovanni config page

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,5 +1,26 @@
 const configBackButton = document.getElementById('config-back-button');
+const exitAccount = document.getElementById('exit-account');
+const removeAccount = document.getElementById('remove-account');
 
 configBackButton.addEventListener("click", () => {
     window.location.href = "home.html";
 });
+
+
+const removeUser = () => {
+    const user = localStorage.getItem('user');
+    if (user) {
+        localStorage.removeItem('user');
+        backToLogin();
+        return user;
+    } else {
+        return null; // No user found in local storage
+    }
+};
+
+const backToLogin = () => {
+    window.location.href = "../index.html";
+};
+
+exitAccount.addEventListener("click", backToLogin);
+removeAccount.addEventListener("click", removeUser);
